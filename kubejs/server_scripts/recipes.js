d43467925f96b68cb89de2f5812f4a285eb86742
Mendaxx <iriseye.marblemellow@gmail.com>
2025-08-14 03:08:22 -0400

    Update recipes.js

--- kubejs/server_scripts/recipes.js
+++ kubejs/server_scripts/recipes.js
@@ -11,7 +11,49 @@ ServerEvents.recipes(e => {
         B: 'minecraft:breeze_rod',  //arg 3: the mapping object
         C: 'minecraft:honeycomb'
     }
-    )
+    ),
+    e.remove({ id: 'minecraft:jukebox' }),
+    e.shaped(
+        Item.of('minecraft:jukebox', 1), 
+        [
+            'PPP',
+            'PAP', 
+            'PPP'
+        ],
+    {
+        P: '#minecraft:planks',
+        A: 'minecraft:amethyst_shard',
+    }
+    ),
+    e.remove({ id: 'minecraft:noteblock' }),
+    e.shaped(
+        Item.of('minecraft:noteblock', 1), 
+        [
+            'PRP',
+            'ARA', 
+            'PRP'
+        ],
+    {
+        P: '#minecraft:planks',
+        A: 'minecraft:amethyst_shard',
+        R: 'minecraft:redstone',
+
+    }
+    ),
+    e.remove({ id: 'etched:etching_table' }),
+    e.shaped(
+        Item.of('etched:etching_table', 1), 
+        [
+            ' AI',
+            'PPP', 
+            '   '
+        ],
+    {
+        A: 'minecraft:amethyst_shard',
+        I: 'minecraft:iron_ingot',  
+        P: '#minecraft:planks'
+    }
+    ),
 
   console.log('Hello! The recipe event has fired!')
 })
